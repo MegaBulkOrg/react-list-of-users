a230dfc54a1c506fbf7b333df33e2bd0fc22ea37
MegaBulkOrg <sheng.bidebao@gmail.com>
2024-02-27 16:27:57 +0300

    Testing headers in API for images

--- src/redux/api/img.ts
+++ src/redux/api/img.ts
@@ -16,7 +16,10 @@ export const uploadApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: `http://${SITE}:${PORT}`,
     prepareHeaders: (headers) => {
+      headers.set('Purivet ID', '345')
+      // headers.set('Access-Control-Allow-Origin', '*')
       headers.set('Access-Control-Allow-Origin', 'http://react.megabulk.ru:1000')
+      console.log(headers);
     }
   }),
   endpoints: (build) => ({
